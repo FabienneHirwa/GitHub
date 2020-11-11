9b8cc3c404c6b834093b12a43adaf48fc9998541
Fabienne Hirwa <fabiennehirwa1@gmail.com>
2020-11-11 14:41:20 -0500

    first commit

---
+++ src/app/scripts.js
@@ -0,0 +1,9 @@
+
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 60)   
+    {
+        $('.nav.opaque-navbar').addClass('.opaque');
+    } else {
+        $('.nav.opaque-navbar').removeClass('.opaque');
+    }
+});
