c1e678da2e8806c5f49b23089812e8f34ede9724
Fabienne Hirwa <fabiennehirwa1@gmail.com>
2020-11-12 04:38:03 -0500

    Auto-generated commit

--- src/app/scripts.js
+++
@@ -1,9 +0,0 @@
-
-  $(window).scroll(function() {
-    if($(this).scrollTop() > 60)   
-    {
-        $('.nav.opaque-navbar').addClass('.opaque');
-    } else {
-        $('.nav.opaque-navbar').removeClass('.opaque');
-    }
-});
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: "https://api.github.com/",
-  accessToken:'fbed96dd2bba0f707d02ec2e3ccf732803a76eaf'
-};
